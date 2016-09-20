b81171f65342203ffac9f8cd8375840faba346ab
Alexandra Lopez <sccp.alopez@gmail.com>
2016-09-20 17:19:23 -0500

    validacion de boton

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 window.addEventListener("load", function() {
     var btn = document.getElementById("enviar");
-
+    var cajaTexto = document.getElementById("contenedor");
+    var contaCaracter = document.getElementById("contador");
+    
     btn.addEventListener("click", function (e){
     	e.preventDefault();
 
-    	var publicacion = document.getElementById("contenedor");
-    	mensajes(publicacion.value);
- 		publicacion.value = "";
+    	mensajes(cajaTexto.value);
+ 		cajaTexto.value = "";
 	});
+    cajaTexto.addEventListener("keyup", function(){
+    	validarBoton(cajaTexto);
 
+    	contarTexto(cajaTexto);
+    });
 	function mensajes (texto) {
     	var contenedorMensaje = document.createElement("div");
 		contenedorMensaje.innerHTML = texto;
@@ -16,5 +21,18 @@ window.addEventListener("load", function() {
     	mensajes.insertBefore(contenedorMensaje, mensajes.childNodes[0]);
 
     	contenedorMensaje.classList.add("globoTexto");
+	}
+
+	function validarBoton(campo){
+		if(campo.value.length==0 || campo.value.length > 140){
+			btn.disabled=true;
+		}
+		else
+			btn.disabled=false;
+	}
+
+	function contarTexto(caja){
+		var caracteres = caja.value.length;
+		contaCaracter.innerText = 140 - caracteres;
 	}
 });
